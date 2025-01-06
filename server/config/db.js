748659ab642f7411471ea75fc9a3673e443a98f6
Lorien Chan <waitingchan@wais-air.home>
2025-01-06 16:16:18 +0000

    file clean up

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,19 @@
-in
+import mongoose from 'mongoose';
+
+// Function to connect to the database
+export const connectDB = async () => {
+    try {
+        const mongoURI = process.env.MONGO_URI;  // Get the MongoDB URI from the environment variable
+        console.log("Mongo URI:", mongoURI);     // Debug log to check if it's loaded correctly
+
+        const connect = await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`Database connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect to the database
+connectDB();
 
 // Routes
 app.get('/', (req, res) => {

49d983663f4213e4de74c4af52362703480cc532
Lorien Chan <lorienting@gmail.com>
2025-01-15 11:54:37 +0000

    Update UserRouter and Auth middleware for bug fixes and improvements

--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { registerUser } from '../Controllers/UserController.js';
+import { registerUser, loginUser, updateUserProfile } from '../Controllers/UserController.js';
+import { protect} from "../middlewares/Auth.js";
 
 const router = express.Router ();
 
 // Public Routes //
 
 router.post("/", registerUser);
+router.post("/login", loginUser);
+
+// Private Route//
+
+router.put( "/", protect, updateUserProfile);
 
 export default router;
--- server/middlewares/Auth.js
+++ server/middlewares/Auth.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import User from "../Models/UserModels.js";
+import asyncHandler from "express-async-handler";
 
 //authenticated user and get token
 const generateToken = (id) => {
@@ -7,4 +9,40 @@ const generateToken = (id) => {
     });
 };
 
-export { generateToken};
+// Protection middleware
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    // Check if token exists in headers
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Extract token
+            token = req.headers.authorization.split(" ")[1];
+
+            // Verify token and decode user ID
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Fetch user and attach to request object
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next(); // Proceed to the next middleware or route handler
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error("Not authorized, token failed");
+        }
+    }
+
+    // If token is missing, return error
+    if (!token) {
+        res.status(401);
+        throw new Error("Not authorized, no token");
+    }
+});
+
+
+
+export { generateToken, protect};

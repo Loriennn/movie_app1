13b4d9f5c29542c2c420e1d7900cb85996b8fba9
Lorien Chan <lorienting@gmail.com>
2025-03-18 22:51:01 +0000

    Update frontend with routing and remove test file

--- Client/frontend/src/App.js
+++ Client/frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'; 
-import MovieDetails from './pages/MovieDetails'; 
-import Navbar from './components/Navbar'; 
-import Footer from './components/Footer'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="flex flex-col min-h-screen">
+                <Navbar />
+                <main className="flex-grow">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/movies/:id" element={<MovieDetails />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;

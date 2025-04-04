0a68544a5acd37885f4ac40b7123536ce522d9ca
Lorien Chan <lorienting@gmail.com>
2025-04-04 23:42:15 +0100

    Updated user model and frontend routing

--- server/Models/UserModels.js
+++ server/Models/UserModels.js
@@ -1,49 +1,52 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const UserSchema = mongoose.Schema(
-    {
-        fullName: {
-            type: String,
-            required: [true, "Add your full name"],
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: [true, "Add an email"],
-            unique: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: [true, "Please add a password"],
-            minlength: [6, "Password must be at least 6 characters"],
-        },
-        image: {
-            type: String,
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        },
-        likeMovies: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Movie",
-                unique: true, // prevent from adding movies
-            },
-        ],
+// Define what a User looks like in the database
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Please enter your full name"],
+      trim: true, // removes spaces before/after
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true, // no duplicate emails allowed
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    image: {
+      type: String, // URL or path to the user's profile image
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false, // regular user by default
+    },
+    likeMovies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie", // references the Movie model
+        unique: true, // each movie should only be liked once
+      },
+    ],
+  },
+  {
+    timestamps: true, // automatically adds createdAt and updatedAt fields
+  }
 );
 
-// 
+// Before saving the user, hash the password if it's new or changed
 UserSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+  if (!this.isModified("password")) return next(); // skip if password hasn't changed
+
+  this.password = await bcrypt.hash(this.password, 10); // hash the password
+  next(); // continue saving
 });
 
+// Export the User model
 export default mongoose.model("User", UserSchema);
